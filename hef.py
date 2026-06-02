c7f3d6fa47ff0b499c774d424847e8bc378224aa
SivamathiRaj19 <balawgon.r@gmail.com>
2026-06-02 13:30:55 +0530

    chore: update HEF model path to scrfd_50m and remove redundant whitespace

--- hef.py
+++ hef.py
@@ -7,11 +7,10 @@ Gst.init(None)
 
 RTSP_URL="rtsp://admin:Tiandy%40123@192.168.1.103:554/Streaming/Channels/101" 
 PP_PATH = "./models/libscrfd.so"
-HEF_PATH = "./models/scrfd_500m.hef"
+HEF_PATH = "./models/scrfd_50m.hef"
 FE_PATH = "./models/arcface_mobilefacenet-2.hef"
 PO_PATH = "./models/libface_recognition_post.so"
 CROPPPER_PATH = "./models/libvms_croppers.so"
-
 
 
 pipeline_str = f"""
