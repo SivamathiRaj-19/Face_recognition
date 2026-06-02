2bf627a5670c3b6d762535cb675b440b46b52a0d
SivamathiRaj19 <balawgon.r@gmail.com>
2026-06-02 13:56:27 +0530

    feat: implement dual-stage Hailo pipeline with detection and recognition, and add standalone RTSP inference wrapper

--- hef.py
+++ hef.py
@@ -5,28 +5,55 @@ from gi.repository import Gst, GLib
 
 Gst.init(None)
 
-RTSP_URL="rtsp://admin:Tiandy%40123@192.168.1.103:554/Streaming/Channels/101" 
-PP_PATH = "./models/libscrfd.so"
-HEF_PATH = "./models/scrfd_500m.hef"
+RTSP_URL = "rtsp://admin:Tiandy%40123@192.168.1.103:554/Streaming/Channels/101"
 
+# PATHS (Ensure these paths match your environment installations)
+DET_HEF = "./models/retinaface_mobilenet_v1.hef" 
+DET_PP  = "./models/libface_detection_post.so" # Added face detection post-process
+REC_HEF = "./models/arcface_mobilefacenet-2.hef"
 
 pipeline_str = f"""
-rtspsrc location={RTSP_URL} latency=100 protocols=tcp !
-rtph264depay ! decodebin ! videoconvert ! videoscale !
-video/x-raw, width=640, height=640, format=RGB ! 
+rtspsrc location={RTSP_URL} latency=100 protocols=udp !
+rtph264depay !
+decodebin !
+videoconvert !
+videoscale !
+video/x-raw,width=640,height=640 !
+queue name=prim_convert_q_sink !
+
+hailonet hef-path={DET_HEF} nms-score-threshold=0.01 nms-iou-threshold=0.03 output-format-type=HAILO_FORMAT_TYPE_FLOAT32 !
 queue !
-hailonet hef-path={HEF_PATH} ! 
+hailofilter so-path={DET_PP} qos=false !
 queue !
-hailofilter so-path={PP_PATH} qos=false ! 
-queue ! 
-hailooverlay ! videoconvert ! autovideosink sync=false
+
+hailocropper name=cropper 
+    hailomuxer name=muxer
+
+cropper.src_0 ! 
+    queue name=hailo_face_rec_q ! 
+    hailonet hef-path={REC_HEF} nms-score-threshold=0.01 nms-iou-threshold=0.03 output-format-type=HAILO_FORMAT_TYPE_FLOAT32 !
+    queue ! 
+    hailofilter so-path={REC_PP} qos=false ! 
+    queue ! 
+    hailoaggregator ! 
+    muxer.sink_0
+
+cropper.src_1 ! 
+    queue name=hailo_bypass_q ! 
+    muxer.sink_1
+
+muxer.src ! 
+    queue !
+    hailooverlay !
+    videoconvert !
+    autovideosink sync=false
 """
 
-
-pipeline=Gst.parse_launch(pipeline_str)
-mainLoop=GLib.MainLoop()
+pipeline = Gst.parse_launch(pipeline_str)
+mainLoop = GLib.MainLoop()
 pipeline.set_state(Gst.State.PLAYING)
-print("pipeline stream is running")
+print("Pipeline stream is running safely...")
+
 try:
     mainLoop.run()
 except KeyboardInterrupt:
