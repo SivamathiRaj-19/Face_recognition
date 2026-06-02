c0f559b3d349d92c427e13af12789db627b25383
SivamathiRaj19 <balawgon.r@gmail.com>
2026-06-02 17:44:44 +0530

    refactor: update model file paths to use relative directory references

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from gi.repository import Gst, GLib
 Gst.init(None)
 
 RTSP_URL = "rtsp://admin:Triton123@192.168.1.103:554/Streaming/Channels/101"
-HEF_PATH = "/home/mandlacx/intern/Face_recognition/models/yolov11n-face.hef"
-PP_PATH ="/home/mandlacx/intern/Face_recognition/models/libyolo_hailortpp_postprocess.so"
+HEF_PATH = "./models/scrfd_500m.hef"
+PP_PATH ="./models/libyolo_hailortpp_postprocess.so"
 HEF2_PATH = ""
 PP2_PATH =""
 
